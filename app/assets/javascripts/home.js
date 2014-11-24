ff50b1ee7411d5b6c22a61b5f5ab518a9de91d8b
ashtindell <tindell.ea@gmail.com>
2014-11-24 18:08:23 -0500

    finished homepage styling

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,8 +1,9 @@
 $(window).on('scroll', function() {
   var scrolled = $(window).scrollTop();
-  $('#slide1').css('top', scrolled * -1.9);
-  $('#slide3').css('top', scrolled * -1.9);
-  $('#slide5').css('top', scrolled * -1.9);
+  // $('#slide1').css('top', scrolled * -1.9);
+  // $('#slide3').css('top', scrolled * -1.9);
+  // $('#slide5').css('top', scrolled * -1.);
+  $('.top-center').css('top', scrolled * 0.8);
 });
 
 
