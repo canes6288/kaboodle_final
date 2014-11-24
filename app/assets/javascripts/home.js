dbd9dc90db26acbf342a9fcff0cba8b84504091d
ashtindell <tindell.ea@gmail.com>
2014-11-24 16:08:29 -0500

    started homepage parallax design

---
+++ app/assets/javascripts/home.js
@@ -0,0 +1,8 @@
+$(window).on('scroll', function() {
+  var scrolled = $(window).scrollTop();
+  $('#slide1').css('top', scrolled * -1.9);
+  $('#slide3').css('top', scrolled * -1.9);
+  $('#slide5').css('top', scrolled * -1.9);
+});
+
+
